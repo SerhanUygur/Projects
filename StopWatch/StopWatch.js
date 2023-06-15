568e56443763b59c541cec2e1c4a56a451cf6ab3
serhan uygur <seuygur55@gmail.com>
2023-06-15 19:32:49 +0300

    Revised

--- StopWatch/StopWatch.js
+++ StopWatch/StopWatch.js
@@ -1,65 +1,82 @@
-const timeDisplay = document.querySelector("#timeDisplay")
-const startBtn = document.querySelector(".StartBtn")
-const stopBtn = document.querySelector(".StopBtn")
-const resetBtn = document.querySelector(".ResetBtn")
+const StartBTN = document.querySelector(".StartBtn")
+const StopBTN = document.querySelector(".StopBtn")
+const ResetBTN = document.querySelector(".ResetBtn")
+let TimeDisplay = document.querySelector("#timeDisplay")
+const LapBTN = document.querySelector(".LapBtn")
+let Laps = document.querySelector(".Laps")
+let resetLaps = document.querySelector("#resetLapBtn")
 
-
-
-let startTime = 0;
-let elapsedTime = 0;
-let paused = true;
-let IntervalId;
+let seconds=0;
+let interval=null;
 let hrs = 0;
-let min = 0;
-let secs = 0; 
+let mins = 0;
+let secs = 0;
 let milisec = 0;
+let pause = true
 
-startBtn.addEventListener("click",()=>{
-    if(paused){
-        paused=false;
-        startTime=Date.now()-elapsedTime
-        IntervalId = setInterval(updateTime,100)  
+
+function timeCount(){
+    milisec +=1
+    if(milisec==100){
+    seconds += 1
+     milisec = 0
+    }
+    if(milisec.toString().length==1){
+        return "0"+ milisec
     }
 
-})
+    hrs = Math.floor(seconds/3600)
+    mins = Math.floor((seconds-(hrs*60*60))/60)
+    secs = Math.floor(seconds % 60)
 
-stopBtn.addEventListener("click", ()=>{
-        if(!paused){
-            paused=true
-            elapsedTime = Date.now()-startTime
-            clearInterval(IntervalId)
+    function pad(nun){
+        if(nun>9){
+            return nun
+        }else if(nun<10){
+            return "0"+nun
         }
-})
+    }
+    secs = pad(secs)
+    mins = pad(mins)
+    hrs = pad(hrs)
+    // milisec=pad(milisec)
 
-resetBtn.addEventListener("click",()=>{
-    paused=true
-    clearInterval(IntervalId)
-     startTime = 0;
-     elapsedTime = 0;
-     currentTime = 0;
-     hrs = 0;
-     min = 0;
-     secs = 0;
-     milisec=0;
-     timeDisplay.innerHTML="00:00:00:00" 
-        
-})
-
-function updateTime(){
-        elapsedTime=Date.now()-startTime      
-        secs = Math.floor((elapsedTime/1000) % 60)
-        min = Math.floor((elapsedTime/(1000 * 60)) %60)
-        hrs = Math.floor((elapsedTime/(1000 * 60 * 60)) %60)
-        milisec = Math.floor((elapsedTime/(100)) %60)
-    
-        secs = pad(secs)
-        min = pad(min)
-        hrs = pad(hrs)
-        milisec=pad(milisec)
-
-        timeDisplay.innerHTML=`${hrs}:${min}:${secs}:${milisec}`
-        
-        function pad(unit){
-            return (("0") + unit).length > 2 ? unit : "0" + unit
-        }
+    TimeDisplay.innerHTML=`${hrs}:${mins}:${secs},${milisec}`
 }
+
+StartBTN.addEventListener("click",()=>{
+    if(pause){
+    pause=false;
+    interval=setInterval(timeCount,(10))
+    }
+})
+
+StopBTN.addEventListener("click",()=>{
+    if(!pause){
+        pause=true
+    clearInterval(interval)
+}
+})
+
+ResetBTN.addEventListener("click",()=>{
+    pause=true
+    hrs = 0
+    mins= 0
+    secs= 0
+    seconds=0
+    milisec=0
+    TimeDisplay.innerHTML =`${"0"+hrs}:${"0"+mins}:${"0"+secs},${"0"+milisec}`
+    clearInterval(interval)
+})
+
+LapBTN.addEventListener("click",()=>{
+    if(!pause){
+        let listItem = document.createElement("li")
+        listItem.innerHTML = TimeDisplay.innerHTML
+        Laps.append(listItem)
+    }
+})
+resetLaps.addEventListener("click",()=>{
+    Laps.innerHTML=""
+})
+
